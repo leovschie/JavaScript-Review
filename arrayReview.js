c21d63c0789954fa84fc62a17c5c2005fb43d3e5
Leo van Schie <leovschie@Leos-MacBook-Pro.local>
2019-05-08 22:14:57 +0200

    Finished Arrays, started objects

--- arrayReview.js
+++ arrayReview.js
@@ -2,9 +2,12 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+function last(){
+  return threeItems.slice(-1).pop();
+};
 
-asdf
+alert(last());
+
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -13,7 +16,15 @@ asdf
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+let allEven = []
+
+for (let i = 0; i < evenArray.length; i++) {
+  if (evenArray[i]%2 === 0) {
+allEven.push(evenArray[i])
+  }
+}
+
+console.log(allEven);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -21,12 +32,21 @@ var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
 
 //below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+let randomNumber = getRandomArbitrary();
+console.log(randomNumber);
+
+if(randomArray.includes(randomNumber)){
+  alert("true");
+} else {
+  alert("false");
+}
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -38,13 +58,14 @@ var first = [1,2,3,4,5];
 var second;
 
   //code here
+  second = [...first];
+  second.push(6,7);
 
-alert(first) //[1,2,3,4,5];
-alert(second) //[1,2,3,4,5,6,7];
+  alert(first) //[1,2,3,4,5];
+  alert(second) //[1,2,3,4,5,6,7];
 
 
-
-/* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
+  /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
@@ -52,6 +73,14 @@ alert(second) //[1,2,3,4,5,6,7];
 var sentence = "Dev Mountain is the best"
 
   //code here
+
+  function longest(string) {
+    const sentenceArray = string.split(' ');
+    const orderedSentenceArray = sentenceArray.sort((a,b) => {
+      return a.length < b.length;
+    });
+    return orderedSentenceArray[0]
+  };
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -64,6 +93,17 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
   //code here
 
+function capitalize(string) {
+  let stringArray = string.split(' '); 
+  let newStringArray = [];
+  stringArray.forEach(word => {
+    newStringArray.push(word[0].toUpperCase() + word.slice(1));
+  });
+  return newStringArray.join(' ');
+}
+capitalize(myPoem)
+
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -72,3 +112,18 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+function vowelCounter(string) {
+  let vowels = 'aeiouAEIOU';
+  let vowelCount = 0;
+  for(let i = 0; i < string.length; i++) {
+    if (vowels.indexOf(string[i]) !== -1) {
+      vowelCount += 1;
+    }
+  
+  }
+  return vowelCount;
+}
+console.log(vowelCounter(theOdyssey));
--- objectReview.js
+++ objectReview.js
@@ -1,10 +1,21 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
   //Your code here
+  let favoriteThings = {
+    band: "The Kills",
+    food: "Indian",
+    person: "Charlotte",
+    book: "The Wind-Up Bird Chronicle",
+    movie: "The Lord of the Rings",
+    holiday: "Christmas"
+  }
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Your code here
+
+  favoriteThings.car = "Pussy Wagon";
+  favoriteThings.brand = "COS";
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
